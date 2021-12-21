79937e11e79d9c51bbf669cee5ce9feae6d1150e
Fred Bradley <hello@fredbradley.co.uk>
2021-12-21 15:35:33 +0000

    Apply fixes from StyleCI (#2)

--- resources/lang/en/questions.php
+++ resources/lang/en/questions.php
@@ -3,5 +3,5 @@
 return [
     '1' => 'Did you manage to do everything you wanted to do? Yes / No, why not?',
     '2' => 'How easy was it for you to accomplish the task you were trying to do today?',
-    '3' => 'What else would you like to see on the '.config('app.name').'?'
+    '3' => 'What else would you like to see on the '.config('app.name').'?',
 ];
